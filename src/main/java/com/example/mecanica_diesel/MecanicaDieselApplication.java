2d24a4491dfe8dcadfcc4e1fbceff0e6072c7398
Leonardo Alves da Silva <leoal_ves@hotmail.com>
2026-04-13 17:18:20 -0300

    Commit com as configurações iniciais e dependencias do spring

---
+++ src/main/java/com/example/mecanica_diesel/MecanicaDieselApplication.java
@@ -0,0 +1,13 @@
+package com.example.mecanica_diesel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MecanicaDieselApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MecanicaDieselApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mecanica_diesel/MecanicaDieselApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mecanica_diesel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MecanicaDieselApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
